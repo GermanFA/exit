64a10a445fed43624438f00b6b29acbdc508a9ed
GermanFA <79627686+GermanFA@users.noreply.github.com>
2021-03-01 12:59:53 +0300

    Добавьте файлы проекта.

---
+++ exit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Привет";
+
+            Console.WriteLine(str);
+
+            while (str != "exit")
+
+            {
+                Console.WriteLine("Введите слово:");
+                str = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
